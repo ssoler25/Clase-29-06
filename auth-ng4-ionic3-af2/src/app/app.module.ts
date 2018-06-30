694e22ae98ab04f422054d1af5c6c2c1c6be9be2
Salas Didacticas Sede Bogota <salas@unilibre.edu.co>
2018-06-29 21:05:23 -0500

    Clase 29 de Junio

---
+++ auth-ng4-ionic3-af2/src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCkWCMNZ7mmciaB_ZdNpKqov4BTGrl6eHw",
+  authDomain: "nuevo-57a3b.firebaseapp.com",
+  databaseURL: "https://nuevo-57a3b.firebaseio.com",
+  projectId: "nuevo-57a3b",
+  storageBucket: "nuevo-57a3b.appspot.com",
+  messagingSenderId: "966989978852"
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    AngularFireDatabase,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ auth-ng4-ionic3-af2/src/pages/home/home.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  displayName;  
+
+  constructor(public navCtrl: NavController,
+    private afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(user => {
+      if (!user) {
+        this.displayName = null;        
+        return;
+      }
+      this.displayName = user.displayName;      
+    });
+  }
+
+  signInWithFacebook() {
+    this.afAuth.auth
+      .signInWithPopup(new firebase.auth.FacebookAuthProvider())
+      .then(res => console.log(res));
+  }
+
+  signOut() {
+    this.afAuth.auth.signOut();
+  }
+
+}
